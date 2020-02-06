b21fa781f3dfcb164716b7bec41aa1453f8e67c3
Takmin <jtm0609@naver.com>
2020-02-06 12:38:45 +0900

    new project

---
+++ Waist_guard_Project/app/src/main/java/com/jtmcompany/waist_guard_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jtmcompany.waist_guard_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
